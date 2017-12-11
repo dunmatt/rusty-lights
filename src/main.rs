63541c01cf26d985731d4a64bdde63d129fccad0
M@ Dunlap <matthew.dunlap@gmail.com>
2017-12-10 22:18:19 -0500

    figured out how to compile the board crate correctly, removed interrupt handler hack

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-#![feature(lang_items)]
 #![feature(proc_macro)] // <- IMPORTANT! Feature gate for procedural macros
-#![feature(used)]
 #![no_std]
 
 // #[macro_use]
@@ -9,7 +7,6 @@ extern crate cortex_m_rt;
 extern crate cortex_m_rtfm as rtfm;
 extern crate stm32f40x;
 
-use cortex_m::asm;
 use cortex_m::peripheral::SystClkSource;
 use rtfm::{app, Threshold};
 
@@ -43,14 +40,4 @@ fn idle() -> ! {
 
 fn toggle(_t: &mut Threshold, r: SYS_TICK::Resources) {
   r.GPIOC.odr.modify(|r, w| w.odr13().bit(!r.odr13().bit()));
-}
-
-// As we are not using interrupts, we just register a dummy catch all handler
-#[allow(dead_code)]
-#[used]
-#[link_section = ".vector_table.interrupts"]
-static INTERRUPTS: [extern "C" fn(); 82] = [default_handler; 82];
-
-extern "C" fn default_handler() {
-    asm::bkpt();
 }
